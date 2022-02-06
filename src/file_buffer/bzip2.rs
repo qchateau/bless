7f91a85770f8fa88d8f6b824aa09e668294e7e5f
Quentin Chateau <quentin.chateau@gmail.com>
2022-02-07 00:15:16 +0100

    optimizations

--- src/file_buffer/bzip2.rs
+++ src/file_buffer/bzip2.rs
@@ -1,7 +1,7 @@
 use super::raw;
 use bzip2::Decompress;
 use regex::bytes::Regex;
-use std::{fs::File, io, io::Read, ops::Range};
+use std::{fmt, fs::File, io, io::Read, ops::Range};
 
 const BLOCK_MAGIC: &'static [u8] = &[0x31, 0x41, 0x59, 0x26, 0x53, 0x59];
 const BUFFER_SIZE: usize = 0xffff;
@@ -13,11 +13,21 @@ fn lstrip_to_magic(mut data: &[u8]) -> &[u8] {
     return data;
 }
 
-#[derive(Debug)]
 pub struct FileBuffer {
     raw_buffer: raw::FileBuffer,
     header: Vec<u8>,
     decoded: Vec<u8>,
+    decoder: Decompress,
+}
+
+impl fmt::Debug for FileBuffer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("FileBuffer")
+            .field("header", &self.header)
+            .field("raw_buffer", &self.raw_buffer)
+            .field("decoded.len", &self.decoded.len())
+            .finish()
+    }
 }
 
 impl FileBuffer {
@@ -29,40 +39,45 @@ impl FileBuffer {
             raw_buffer: raw::FileBuffer::new(path)?,
             header,
             decoded: Vec::new(),
+            decoder: Decompress::new(false),
         });
     }
     pub fn is_valid(&self) -> bool {
         return Regex::new("BZ[h0][1-9]").unwrap().is_match(&self.header);
     }
 
-    fn decode(&mut self) -> io::Result<()> {
-        let mut decoder = Decompress::new(false);
-        let mut data = lstrip_to_magic(super::FileBuffer::data(&mut self.raw_buffer));
-        if data.len() < BLOCK_MAGIC.len() {
+    fn reset_decoder(&mut self) {
+        self.decoder = Decompress::new(false);
+    }
+
+    fn incremental_decode(&mut self) -> io::Result<()> {
+        if self.decoder.total_in() == 0 {
             self.decoded.clear();
-            return Ok(());
+            self.decoder
+                .decompress(self.header.as_slice(), &mut self.decoded[0..0])?;
         }
+
+        let mut data = lstrip_to_magic(super::FileBuffer::data(&mut self.raw_buffer));
 
         // remove the last char, it's irrelevant in most cases, and we *never*
         // want the decoder to encounter the end of the stream
-        data = &data[..data.len() - 1];
+        let offset = self.decoder.total_in() as usize - self.header.len();
+        data = data.get(offset..data.len() - 1).unwrap_or(b"");
 
-        let mut buf = Vec::new();
-        decoder.decompress_vec(self.header.as_slice(), &mut buf)?;
         loop {
-            let before_in = decoder.total_in();
-            let before_out = decoder.total_out();
-            buf.reserve(buf.len() + BUFFER_SIZE);
-            decoder.decompress_vec(data, &mut buf)?;
-            let consumed = decoder.total_in() - before_in;
-            let produced = decoder.total_out() - before_out;
+            let before_in = self.decoder.total_in();
+            let before_out = self.decoder.total_out();
+            self.decoded.reserve(self.decoded.len() + BUFFER_SIZE);
+            self.decoder.decompress_vec(data, &mut self.decoded)?;
+            let consumed = self.decoder.total_in() - before_in;
+            let produced = self.decoder.total_out() - before_out;
 
             data = &data[consumed as usize..];
             if produced == 0 && consumed == 0 {
                 break;
             }
         }
-        self.decoded = buf;
+
         return Ok(());
     }
 }
@@ -79,25 +94,32 @@ impl super::FileBuffer for FileBuffer {
     }
     fn jump(&mut self, bytes: u64) {
         self.raw_buffer.jump(bytes);
+        self.reset_decoder();
         self.decoded.clear();
     }
     fn load_prev(&mut self) -> std::io::Result<usize> {
         let size_before = self.decoded.len();
-        while self.decoded.len() == size_before {
-            self.raw_buffer.load_prev()?;
-            self.decode().ok();
+        while self.decoded.len() <= size_before {
+            if self.raw_buffer.load_prev()? == 0 {
+                return Ok(0);
+            }
+            self.reset_decoder();
+            self.incremental_decode()?;
         }
         return Ok(self.decoded.len() - size_before);
     }
     fn load_next(&mut self) -> std::io::Result<usize> {
         let size_before = self.decoded.len();
-        while self.decoded.len() == size_before {
-            self.raw_buffer.load_next()?;
-            self.decode().ok();
+        while self.decoded.len() <= size_before {
+            if self.raw_buffer.load_next()? == 0 {
+                return Ok(0);
+            }
+            self.incremental_decode()?;
         }
         return Ok(self.decoded.len() - size_before);
     }
     fn shrink_to(&mut self, range: Range<u64>) {
+        self.reset_decoder();
         return self.raw_buffer.shrink_to(range);
     }
 }
---
+++ src/file_buffer/devec.rs
@@ -0,0 +1,67 @@
+use std::{cmp::max, fmt};
+
+pub struct DeVec<T> {
+    data: Vec<T>,
+    offset: usize,
+}
+
+impl<T> DeVec<T> {
+    pub fn new() -> DeVec<T> {
+        DeVec {
+            data: Vec::new(),
+            offset: 0,
+        }
+    }
+    pub fn len(&self) -> usize {
+        return self.data.len() - self.offset;
+    }
+    pub fn as_slice(&self) -> &[T] {
+        return &self.data.as_slice()[self.offset..];
+    }
+    pub fn as_mut_slice(&mut self) -> &mut [T] {
+        return &mut self.data.as_mut_slice()[self.offset..];
+    }
+    pub fn clear(&mut self) {
+        self.data.truncate(self.offset)
+    }
+    pub fn shrink_to(&mut self, size: usize) {
+        let size = max(size, self.len());
+        let max_offset = size / 2;
+        if self.offset > max_offset {
+            let shift = self.offset - max_offset;
+            self.data.rotate_left(shift);
+            self.data.truncate(self.data.len() - shift);
+            self.offset = max_offset;
+        }
+        self.data.shrink_to(size);
+    }
+}
+
+impl<T: Default + Clone> DeVec<T> {
+    pub fn resize(&mut self, size: usize) {
+        self.data.resize(size + self.offset, T::default());
+    }
+    pub fn resize_front(&mut self, size: usize) {
+        let len = self.len();
+        if size > len {
+            let extra = size - len;
+            let missing = extra.saturating_sub(self.offset);
+            self.offset = self.offset.saturating_sub(extra);
+            if missing > 0 {
+                self.data.resize(self.data.len() + missing, T::default());
+                self.data.rotate_right(missing);
+            }
+        } else {
+            self.offset += len - size;
+        }
+    }
+}
+
+impl<T> fmt::Debug for DeVec<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("DeVec")
+            .field("data.len", &self.data.len())
+            .field("offset", &self.offset)
+            .finish()
+    }
+}
