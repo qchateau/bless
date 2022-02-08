07c4a4cb9f651d5f050935362eb56f9a258f69d9
Quentin Chateau <quentin.chateau@gmail.com>
2022-02-08 14:03:02 +0100

    fix stderr problem

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ mod utils;
 
 use crate::file_view::BufferedFileView;
 use clap::Parser;
-use std::io;
+use gag::Redirect;
+use std::{fs, io};
 
 #[derive(Parser)]
 struct Args {
@@ -16,6 +17,22 @@ struct Args {
 
 fn main() -> io::Result<()> {
     let args = Args::parse();
+    let _redirect_stderr = if atty::is(atty::Stream::Stderr) {
+        Some(
+            Redirect::stderr(
+                fs::OpenOptions::new()
+                    .truncate(true)
+                    .read(true)
+                    .create(true)
+                    .write(true)
+                    .open("/dev/null")
+                    .unwrap(),
+            )
+            .unwrap(),
+        )
+    } else {
+        None
+    };
     return match BufferedFileView::new(args.path) {
         Ok(file_view) => ui::run(Box::new(file_view)),
         Err(err) => Err(err),
